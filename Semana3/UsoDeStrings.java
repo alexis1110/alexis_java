e062d06dfe655e666337a0842abe33ab9cfef6b2
alexis <202300704b@utea.edu.pe>
2024-06-17 09:40:36 -0500

    1

---
+++ Semana3/UsoDeStrings.java
@@ -0,0 +1,15 @@
+package Semana3;
+
+import javax.print.DocFlavor.STRING;
+
+public class UsoDeStrings {
+    
+   public static void main(String[] args) {
+    String palabra = "Cerveza";
+    String frase = "¿donde esta mi cerveza?";
+
+    System.out.println("palabra que uso con frecuencia:"+palabra);
+    System.out.println("una frase que uso aveces:"+frase);
+   }
+    
+}
