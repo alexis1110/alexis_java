96073798472d360027a3c4333e54a8ee94da2b3f
alexis <202300704b@utea.edu.pe>
2024-06-20 12:55:36 -0500

    j

--- Semana3/Ejercicios1.java
+++ Semana3/Ejercicios1.java
@@ -2,8 +2,14 @@ package Semana3;
 
 public class Ejercicios1 {
 
-    public static void main(String[] a
+    public static void main(String[] args){
 
-    }
+    int euros;
+    double soles; 
+
+    System.out.println("ingrese un monto en euros");
+    euros = Integer.parseInt(System.console().readLine()); 
+    soles = euros*4.09;
+System.out.println("el valor en soles es"+soles);}
     
 }
