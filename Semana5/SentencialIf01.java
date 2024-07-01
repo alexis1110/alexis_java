6d491af2dfe58ecb665df2a5775fd517182bc691
alexis <202300704b@utea.edu.pe>
2024-07-01 08:50:26 -0500

    j

---
+++ Semana5/SentencialIf01.java
@@ -0,0 +1,15 @@
+package Semana5;
+
+public class SentencialIf01 {
+    
+public static void main(String[] args) {
+    String respuesta = "";
+    System.out.println("¿cual es la capital de venesuela?");
+    respuesta = System.console().readLine();
+    if (respuesta.equals("caracas")) {
+        System.out.println("eres un genio, debes estar en sistemas");
+        }else{
+            System.out.println("Recoje tu pico y pala, ve ala chacra");
+        }
+}
+}
